b8f774ef2bf354ce163a39baecbaec9b97224e95
ro53viwe <marzieh.asalian@fau.de>
2023-11-12 12:53:11 +0100

    changed variable name and function, put some comments, wrote Docstring, put error handleing, modify it according to tests_math_quiz for fix Bugs

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,100 @@
+#Marzieh Asalian
 import random
 
+# Generate_random_number generates a random integer between min and max and include this max and min
+# for example: if max:3 and min:1 randomly this function pic two number from 1,2,3
 
-def function_A(min, max):
+def Generate_random_number(min_random, max_random):
     """
-    Random integer.
+        Generate_random_number
+
+        Parameters:
+        - min_random (int): The max number.
+        - min_random (int): The min number.
+
+        Returns:
+        int: a random int between min_random and  max_random
+        """
+    return random.randint(min_random, max_random)
+
+def Generate_random_operator():
     """
-    return random.randint(min, max)
+            Generate_Random_operator
+            Parameters: nothing
 
+            Returns:
+            str: Generate_random_operator randomly choose between '+', '-', '*'
+           """
 
-def function_B():
     return random.choice(['+', '-', '*'])
 
+# Generate_random_Function: with random numbers from Generate_random_number and random sign form Generate_random_operator creat math problem
+#calculate the correct answer for that math problem
+def Generate_random_Function(first_random_number, second_random_number, Random_operator):
+    """
+        Generate_random_number
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+        Parameters:
+        - first_random_number (int): random number .
+        - second_random_number (int): random number.
+        - Random_operator (str): random operator.
+
+
+        Returns:
+        random_function(str): a random function
+        correct_answer(int): correct answer of that random function
+        """
+    random_function = f"{first_random_number} {Random_operator} {first_random_number}"
+    if Random_operator == '+':
+        correct_answer = first_random_number + first_random_number
+    elif Random_operator == '-':
+        correct_answer = first_random_number - first_random_number
+    else:
+        correct_answer = first_random_number * first_random_number
+
+    return random_function, correct_answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+            create math quiz:
+            str: get the answer of the math quiz from participant
+            str: print out that if the answer is correct and if is not print out the correct number
+            str: at the end of the game print out if the participant win or not
+            """
+    Counter_correct = 0
+    # put the value of zero for the counting the number of the correct answers
+    # our quiz has ask 3 question, so we put steps_of_quiz=3
+    steps_of_quiz = 3
+    # Initiation message for stating the Game
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
-
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+    for _ in range(steps_of_quiz):
+        #first_random_number:generate first random unt number between 1 and 10, first_random_number: generate second random int number between 1 and 5, and o generate random operation
+        first_random_number = Generate_random_number(1, 10); first_random_number = Generate_random_number(1, 5); Random_operator = Generate_random_operator()
+        #PROBLEM is the random function with that random variable and ANSWER is the correct answer to that function
+        PROBLEM, ANSWER = Generate_random_Function(first_random_number, first_random_number, Random_operator)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        #Error handling: making sure that participants input an integer
+        while True:
+            try:
+                useranswer = input("Your answer: ")
+                useranswer = int(useranswer)
+                break
+            except ValueError:
+                print("your answer is not integer, please enter an integer for answer")
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            Counter_correct += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
-
+    if Counter_correct==steps_of_quiz :
+        print(f"\nYou won! Your score is: {Counter_correct}/{steps_of_quiz}")
+    else:
+        print(f"\nGame over! Your score is: {Counter_correct}/{steps_of_quiz}")
+#Run the math quiz if this code is executed directly
 if __name__ == "__main__":
     math_quiz()
